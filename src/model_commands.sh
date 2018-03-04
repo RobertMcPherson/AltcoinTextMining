ab55930987575de6a063a9cfd6ae18c74ddf3e48
Robert L. McPherson <robert.l.mcpherson@gmail.com>
2018-03-04 16:29:40 -0500

    initial commit

---
+++ src/model_commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#uncomment line below if compiling a new model for the first time
+#javac -cp h2o-genmodel.jar -J-Xmx2g DRF_model_R_1470416938086_15.java 
+#java -cp .:h2o-genmodel.jar hex.genmodel.tools.PredictCsv --header --model DRF_model_R_1470416938086_15 --input input.csv --output output.csv
+#javac -cp h2o-genmodel.jar -J-Xmx2g NewPredictCsv.java GBM_grid_0_AutoML_20180127_161842_model_33.java
+#java -cp .:h2o-genmodel.jar NewPredictCsv --header --model GBM_grid_0_AutoML_20180127_161842_model_33 --input labels_and_vectors.csv --output output.csv
+#java -cp .:ICO_GBM_model_33.jar:h2o-genmodel.jar NewPredictCsv --header --model GBM_grid_0_AutoML_20180127_161842_model_33 --input labels_and_vectors.csv --output output.csv
+java -cp .:DeepLearning_grid_0_AutoML_20180225_012018_model_9.jar:h2o-genmodel.jar NewPredictCsv --header --model DeepLearning_grid_0_AutoML_20180225_012018_model_9 --input labels_and_vectors.csv --output output.csv
+
+
+
+
+
+
+
